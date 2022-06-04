56d4436eb8daf14b25c4655b8f1fe27935c096e5
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-06-04 09:16:56 +0530

    Updating roboshop-shared-library

---
+++ vars/nodejs.groovy
@@ -0,0 +1,24 @@
+def lintChecks() {
+    sh '''
+        # we comment this because devs gonna check the failures
+        #~/node_modules/jslint/bin/jslint.js server.js
+        echo lint Check for ${COMPONENT}
+    '''
+}
+
+def call() {
+    pipeline {
+        agent any
+        stages{
+            // for each commit
+            stage("Lint Checks"){
+                steps {
+                    script{
+                        lintChecks()
+                    }
+
+                }
+            }
+        } // end of stages
+    }
+}
