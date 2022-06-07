53b416219d43363e39372959c91675e81dbd3008
Naren-Chakravarthy <narenchowdary14@gmail.com>
2022-06-07 19:58:01 +0530

    Updating roboshop-shared-library

--- vars/python.groovy
+++ vars/python.groovy
@@ -2,37 +2,20 @@ def lintChecks() {
     sh '''
         # we comment this because devs gonna check the failures
         #~/node_modules/jslint/bin/jslint.js server.js
+        #pylint *.py
         echo lint Check for ${COMPONENT}
-    '''
-}
-
-def SonarCheck() {
-    sh '''
-      sonar-scanner -Dsonar.host.url=http://172.31.3.155:9000 -Dsonar.sources=. -Dsonar.projectkey=${COMPONENT} -Dsonar.login=${SONAR_USR} -Dsonar.password=${SONAR_PSW}
     '''
 }
 
 def call() {
     pipeline {
         agent any
-
-        environment{
-            SONAR = credentials('SONAR')
-        }
         stages{
             // for each commit
             stage("Lint Checks"){
                 steps {
                     script{
                         lintChecks()
-                    }
-
-                }
-            }
-            stage("Sonar Check"){
-                steps {
-                    script{
-                        SonarCheck()
                     }
 
                 }
